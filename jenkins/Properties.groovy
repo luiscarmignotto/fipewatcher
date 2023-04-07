cac1063e6f087d470fda0658d45993bb55f421a8
luiscarmignotto <ikemeneghetti@gmail.com>
2023-04-07 10:34:12 -0300

    Jenkins tests

---
+++ jenkins/Properties.groovy
@@ -0,0 +1,15 @@
+def globalProperties: { 
+    ansible: {
+        playbooks: {
+            branch: 'master',
+            credentials: 'git-repos'
+            repoUrl: 'git@github.com:luiscarmignotto/devops-ansible-k8s-builds.git'
+        },
+        roles: {
+            branch: 'master',
+            credentials: 'git-repos'
+            repoUrl: 'git@github.com:luiscarmignotto/devops-ansible-k8s-roles.git'
+        }
+    }
+
+}
