aee9fe7954e5d569bca0a4b4639cd18adf2379f5
luiscarmignotto <ikemeneghetti@gmail.com>
2023-04-07 10:56:37 -0300

    test

--- jenkins/GlobalProperties.groovy
+++ jenkins/GlobalProperties.groovy
@@ -1,14 +1,14 @@
-def globalProperties = [
-    ansible: [
-        playbooks: [
+def globalProperties = {
+    ansible: {
+        playbooks: {
             branch: 'master',
             credentials: 'git-repos',
             repoUrl: 'git@github.com:luiscarmignotto/devops-ansible-k8s-builds.git'
-        ],
-        roles: [
+        },
+        roles: {
             branch: 'master',
             credentials: 'git-repos',
             repoUrl: 'git@github.com:luiscarmignotto/devops-ansible-k8s-roles.git'
-        ]
-    ]
-]
+        }
+    }
+}
